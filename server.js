34af062a18383deb0bd63e91bfd6437d723ba72c
Quyen Luu <luqu0501@gmail.com>
2022-07-18 22:24:01 +0700

    feat: add page structure & start server

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
+const port = 8000;
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static('src'));
 
 // Setup Server
+app.get('/test', (req, res) => {
+  res.send(projectData);
+});
+
+app.listen(port, () => {
+  console.log(`Now app is running on http://localhost:${port}`);
+});
